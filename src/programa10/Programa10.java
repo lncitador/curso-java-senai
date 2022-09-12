6b02c839b837f798ee32f00d16ef1197b4cb9c1e
Walaff Fernandes <walaffonofre@icloud.com>
2022-09-12 19:48:20 -0300

    aula sobre estrutura de repetição

---
+++ src/programa10/Programa10.java
@@ -0,0 +1,21 @@
+package programa10;
+
+public class Programa10 {
+    public static void main(String[] args) {
+        int contador = 0;
+
+//        while (contador <= 10) {
+//            System.out.println("Contador: " + contador);
+//            contador++;
+//        }
+
+//        do {
+//            System.out.println("Contador: " + contador);
+//            contador++;
+//        } while (contador <= 10);
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Contador: " + i);
+        }
+    }
+}
